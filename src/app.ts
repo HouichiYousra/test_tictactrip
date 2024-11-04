be44bd91a86473f9d448ee896d26614058553d7d
HouichiYousra <gy_houichi@esi.dz>
2024-11-04 14:59:51 +0100

    add auth endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 import router from './routes';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.text());
+app.use(express.json());
 
 app.use('/api', router);
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
 import { justifyText } from './justify';
 
+dotenv.config();
+
 const router = express.Router();
+const JWT_KEY = process.env.JWT_KEY ||'';
 
 router.post('/justify', (req: any, res: any)=> {
     const text = req.body as string;
@@ -12,6 +18,18 @@ router.post('/justify', (req: any, res: any)=> {
 
     const justifiedText = justifyText(text);
     return res.type('text/plain').send(justifiedText);
+});
+
+router.post('/token', (req: any, res: any) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: 'Email is required' });
+    }
+
+    const token = jwt.sign({ email }, JWT_KEY, { expiresIn: '1d' });
+
+    res.json({ token });
 });
 
 export default router;
